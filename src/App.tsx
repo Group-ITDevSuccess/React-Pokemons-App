9c5ce318c4264458172fa4d56191f12f7b911018
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-10 23:17:03 +0300

    Les differentes types de composant

--- src/App.tsx
+++ src/App.tsx
@@ -10,3 +10,15 @@ const App: FunctionComponent = () => {
 }
 
 export default App;
+
+/*
+import React from 'react';
+
+export default class App extends React.Component{
+    const name: string = 'React';
+
+    render(): React.ReactNode {
+        return <h1>Hello, {name}</h1>;
+    }
+}
+*/
