a664b551fb881c7183857d0db011da4c857e5c38
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-10 23:55:34 +0300

    Ajout des models pokemon et son mock

---
+++ src/models/mock-pokemon.ts
@@ -0,0 +1,94 @@
+import Pokemon from "./pokemon";
+
+export const POKEMONS: Pokemon[] = [
+    {
+        id: 1,
+        name: "Bulbasaur",
+        hp: 100,
+        cp: 10,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+        types: ["Grass", "Poison"],
+        created: new Date("2023-05-10")
+    },
+    {
+        id: 2,
+        name: "Charmander",
+        hp: 120,
+        cp: 15,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+        types: ["Fire"],
+        created: new Date("2023-05-10")
+    },
+    {
+        id: 3,
+        name: "Squirtle",
+        hp: 90,
+        cp: 8,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+        types: ["Water"],
+        created: new Date("2023-05-10")
+    },
+    {
+        id: 4,
+        name: "Pikachu",
+        hp: 80,
+        cp: 12,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+        types: ["Electric"],
+        created: new Date("2023-05-10")
+    },
+    {
+        id: 5,
+        name: "Mewtwo",
+        hp: 200,
+        cp: 20,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png",
+        types: ["Psychic"],
+        created: new Date("2023-05-10")
+    },
+    {
+        id: 6,
+        name: "Snorlax",
+        hp: 180,
+        cp: 18,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png",
+        types: ["Normal"],
+        created: new Date("2023-05-10")
+    },
+    {
+        id: 7,
+        name: "Gyarados",
+        hp: 150,
+        cp: 16,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/130.png",
+        types: ["Water", "Flying"],
+        created: new Date("2023-05-10")
+    },
+    {
+        id: 8,
+        name: "Machamp",
+        hp: 140,
+        cp: 14,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/068.png",
+        types: ["Fighting"],
+        created: new Date("2023-05-10")
+    },
+    {
+        id: 9,
+        name: "Alakazam",
+        hp: 110,
+        cp: 13,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/065.png",
+        types: ["Psychic"],
+        created: new Date("2023-05-10")
+    },
+    {
+        id: 10,
+        name: "Dragonite",
+        hp: 170,
+        cp: 19,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/149.png",
+        types: ["Dragon", "Flying"],
+        created: new Date("2023-05-10")
+    },
+];
---
+++ src/models/pokemon.ts
@@ -0,0 +1,29 @@
+export default class Pokemon {
+  // 1. Typage des propriétés d'un pokemon
+  id: number;
+  hp: number;
+  cp: number;
+  name: string;
+  picture: string;
+  types: Array<string>;
+  created: Date;
+
+  // 2. Définition  des valeur par defaut des propriétés d'un pokemon
+  constructor(
+    id: number,
+    hp: number = 100,
+    cp: number = 10,
+    name: string = "name",
+    picture: string = "http://...",
+    types: Array<string> = ["Normal"],
+    created: Date = new Date()
+  ) {
+    this.id = id;
+    this.hp = hp;
+    this.cp = cp;
+    this.name = name;
+    this.picture = picture;
+    this.types = types;
+    this.created = created;
+  }
+}
